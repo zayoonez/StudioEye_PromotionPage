ba53ca067d9eb4e7eec0c690956a4d65f5be7ce4
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2023-10-04 00:35:37 +0900

    feat: insertPage 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {Routes, Route, BrowserRouter, Navigate} from "react-router-dom";
 import PromotionMainpage from "./Pages/MainPage/PromotionMainPage";
+import AboutMainpage from "./Pages/AboutPage/AboutMainPage";
+import { AnimatePresence, motion } from 'framer-motion';
+import InsertPage from "./Pages/InsertPage/InsertPage";
 
 function App() {
+    const [showInsertPage, setShowInsertPage] = React.useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowInsertPage(false);
+        }, 5000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, []);
+
   return (
       <BrowserRouter>
+          <AnimatePresence>
           <Routes>
-              <Route path="/"  element={<PromotionMainpage />}/>
+              <Route
+                  path="/"
+                  element={
+                      showInsertPage ? (
+                          <motion.div
+                              initial={{ opacity: 0 }}
+                              animate={{ opacity: 1 }}
+                              exit={{ opacity: 0, transition: { duration: 0.5 } }}
+                          >
+                              <InsertPage />
+                          </motion.div>
+                      ) : (
+                          <motion.div
+                              initial={{ opacity: 0 }}
+                              animate={{ opacity: 1 }}
+                              exit={{ opacity: 0 }}
+                              transition={{ duration: 0.5 }}
+                          >
+                              <PromotionMainpage />
+                          </motion.div>
+                      )
+                  }
+              />
+              <Route path="/About"  element={<AboutMainpage />}/>
+              <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
+          </AnimatePresence>
       </BrowserRouter>
   );
 }
