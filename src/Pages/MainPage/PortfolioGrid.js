6c856272dcd30dba3296293114580b897c4e5d50
JayoonKim <jayoon999@gmail.com>
2023-10-04 15:47:47 +0900

    feat: 포트폴리오 페이지 생성

---
+++ src/Pages/MainPage/PortfolioGrid.js
@@ -0,0 +1,86 @@
+import React from "react";
+import styled from "styled-components";
+import { motion, useAnimation } from "framer-motion";
+
+import { useInView } from "react-intersection-observer";
+
+import { useEffect } from "react";
+import image1 from "../../assets/images/1.jpeg";
+import image2 from "../../assets/images/2.jpeg";
+import image3 from "../../assets/images/3.jpeg";
+import image4 from "../../assets/images/4.jpeg";
+import image5 from "../../assets/images/5.jpeg";
+
+const BoxContainer = styled(motion.div)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+
+const Item = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50%;
+    width: 300px;
+    /* background-color: gray; */
+    margin: 100px;
+
+    /* position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0; */
+`;
+const Thumbnail  = styled.img`
+    width: 700px;
+    height: 600px;
+`;
+
+const boxVariant = {
+    visible: { opacity: 1, scale: 1, transition : {duration : 0.7} },
+    hidden: { opacity: 0, scale: 0},
+  }
+
+  const Image = ({ id }) => {
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+  
+    useEffect(() => {
+      if (inView) {
+        control.start("visible");
+      } else {
+        control.start("hidden");
+      }
+    }, [control, inView]);
+  
+    const imageSrc = require(`../../assets/images/${id}.jpeg`);
+
+    return (
+         <Item
+          ref = {ref}
+          src={imageSrc}
+          variants={boxVariant}
+          initial="hidden"
+          animate={control}>
+            <Thumbnail src={imageSrc} alt = {'thumnail${id}'}></Thumbnail>
+          </Item>      
+
+    );
+  };
+  
+  export default function PortfolioGrid() {
+    return (
+      <BoxContainer>
+        {[1, 2, 3, 4, 5].map((image) => (
+            <Image key = {image} id={image}/>   
+        ))}
+        {/* <Box num={1} />
+        <Box num={2} />
+        <Box num={3} /> */}
+        
+      </BoxContainer>
+    );
+  }
--- src/Pages/MainPage/PromotionMainPage.js
+++ src/Pages/MainPage/PromotionMainPage.js
@@ -1,15 +1,41 @@
 import React from "react";
 import styled from "styled-components";
 import Body from "../../Components/Common/Body";
+import PortfolioGrid from "./PortfolioGrid"
+import {motion, useScroll} from "framer-motion"
+import {ReactComponent as LogoIcon} from '../../assets/logo/STUDIO-I.svg'
+
 
 const MainBody = styled.div`
     display : flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
+    background-color : white;
+    height: 100vh;
  
-
 `;
+const ProgressBar = styled(motion.div)`
+    position: fixed;
+    top: 4rem;
+    left: 0;
+    right: 0;
+    height: 10px;
+    background-color: black;
+    transform-origin: 0%;
+`;
+const StyledLogoIcon = styled(LogoIcon)`
+    width: 500px;
+    height: 200px;
+    background-color: white;
+`;
+const LogoContainer = styled.div`
+    height: auto;
+    display: flex;
+    /* background-color: white; */
+`;
+
+// const MainLogo = styled.
 
 const Wrapper = styled.div`
     display : flex;
@@ -20,15 +46,24 @@ const Wrapper = styled.div`
     /* max-width: 100%; */
     flex-wrap: wrap; /* 화면 크기가 작아지면 아래로 내려갈 수 있도록 설정 */
 `;
+const variants = {
+    start: { pathLength: 0, fill: "rgba(255, 255, 255,0)" },
+    end: { pathLength: 1, fill: "rgba(255, 255, 255, 1)" }
+  };
 
 const PromotionMainpage = () => {
-
+    const {scrollYProgress} = useScroll();
     const PromotionMainpageContent=()=>{
         return (
             <>
+            <ProgressBar style = {{scaleX:scrollYProgress}}></ProgressBar>
                 <MainBody>
-
+                    <LogoContainer>
+                        <StyledLogoIcon />
+                    </LogoContainer>
                 </MainBody>
+                <PortfolioGrid/>
+
             </>
         )
     }
@@ -37,6 +72,7 @@ const PromotionMainpage = () => {
     return(
         <Body>
             <PromotionMainpageContent/>
+
         </Body>
     )
 }
