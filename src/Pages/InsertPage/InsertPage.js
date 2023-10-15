249fb7a2c2526ddfa0b83c478db866ab451e2856
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2023-10-16 00:10:30 +0900

    chore: 시작 로고 변경

--- src/Pages/InsertPage/InsertPage.js
+++ src/Pages/InsertPage/InsertPage.js
@@ -24,7 +24,7 @@ const InsertPage = () => {
         return (
             <>
                 <MainBody>
-                            <Video src="/videos/studioi.mp4" autoPlay muted/>
+                            <Video src="/videos/text_2.mp4" autoPlay muted/>
                 </MainBody>
             </>
         )
