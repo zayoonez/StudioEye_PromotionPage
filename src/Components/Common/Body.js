a9b8c830a833af9df2c39c701e1f0f6d07fcfa97
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2023-10-16 17:27:52 +0900

    chore: body 구조 수정

--- src/Components/Common/Body.js
+++ src/Components/Common/Body.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styled from 'styled-components';
 import Header from "./header";
 import SideBar from "./SideBar";
