0065c76d3a6097d80b34963bd94ed970ae4bb30a
junmingeun <junminguen@naver.com>
2023-10-24 19:49:51 +0900

    feat: notice page 구현

--- src/Pages/NoticePage/NoticeMainPage.js
+++ src/Pages/NoticePage/NoticeMainPage.js
@@ -4,7 +4,7 @@ import Body from "../../Components/Common/Body";
 import {motion} from "framer-motion";
 import axios from "axios";
 import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
+import MyAccordion from "./Components/MyAccordion";
 
 
 const BoxContainer = styled(motion.div)`
@@ -18,6 +18,7 @@ const BoxContainer = styled(motion.div)`
 const Text = styled(motion.text)`
     font-size: 4rem;
     font-weight: 600;
+    margin-bottom: 2rem;
 `;
 const FaQContainer = styled(motion.div)`
     display: flex;
@@ -88,71 +89,21 @@ const NoticeMainpage = () => {
             <>
                 <BoxContainer>
                     <Text>notice</Text>
-                    <div className="accordion accordion-flush" id="accordionFlushExample">
-                        <div className="accordion-item">
-                            <h2 className="accordion-header" id="flush-headingOne">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseOne" aria-expanded="false"
-                                        aria-controls="flush-collapseOne">
-                                    Accordion Item #1
-                                </button>
-                            </h2>
-                            <div id="flush-collapseOne" className="accordion-collapse collapse"
-                                 aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                <div className="accordion-body">Placeholder content for this accordion, which is
-                                    intended to demonstrate the <code>.accordion-flush</code> class. This is the first
-                                    item's accordion body.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="accordion-item">
-                            <h2 className="accordion-header" id="flush-headingTwo">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseTwo" aria-expanded="false"
-                                        aria-controls="flush-collapseTwo">
-                                    Accordion Item #2
-                                </button>
-                            </h2>
-                            <div id="flush-collapseTwo" className="accordion-collapse collapse"
-                                 aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                                <div className="accordion-body">Placeholder content for this accordion, which is
-                                    intended to demonstrate the <code>.accordion-flush</code> class. This is the second
-                                    item's accordion body. Let's imagine this being filled with some actual content.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="accordion-item">
-                            <h2 className="accordion-header" id="flush-headingThree">
-                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseThree" aria-expanded="false"
-                                        aria-controls="flush-collapseThree">
-                                    Accordion Item #3
-                                </button>
-                            </h2>
-                            <div id="flush-collapseThree" className="accordion-collapse collapse"
-                                 aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                                <div className="accordion-body">Placeholder content for this accordion, which is
-                                    intended to demonstrate the <code>.accordion-flush</code> class. This is the third
-                                    item's accordion body. Nothing more exciting happening here in terms of content, but
-                                    just filling up the space to make it look, at least at first glance, a bit more
-                                    representative of how this would look in a real-world application.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/*<FaQContainer>*/}
-                    {/*    <Accordions defaultActiveKey="0" flush>*/}
-                    {/*        {data.map((item, i) => (*/}
-                    {/*            // <Title>Q{i+1}.{item.title}</Title>*/}
-                    {/*            <Accordion.Item key={item.id} eventKey={i}>*/}
-                    {/*                <Accordion.Header>{item.title}</Accordion.Header>*/}
-                    {/*                <Accordion.Body>*/}
-                    {/*                    <img src={item.imageUrl} />*/}
-                    {/*                </Accordion.Body>*/}
-                    {/*            </Accordion.Item>*/}
-                    {/*        ))}*/}
-                    {/*    </Accordions>*/}
-                    {/*</FaQContainer>*/}
+                        {data.map((item, i) => (
+                            <MyAccordion index={i+1} title={item.title} contents={item.imageUrl} />
+                        ))}
+                    <MyAccordion index={2} title="adsfasd" contents="adsfasd" />
+                        {/*<Accordions defaultActiveKey="0" flush>*/}
+                        {/*    {data.map((item, i) => (*/}
+                        {/*        // <Title>Q{i+1}.{item.title}</Title>*/}
+                        {/*        <AccordionItem key={item.id} eventKey={i}>*/}
+                        {/*            <AccordionHeader>{item.title}</AccordionHeader>*/}
+                        {/*            <AccordionBody>*/}
+                        {/*                <img src={item.imageUrl} />*/}
+                        {/*            </AccordionBody>*/}
+                        {/*        </AccordionItem>*/}
+                        {/*    ))}*/}
+                        {/*</Accordions>*/}
                 </BoxContainer>
             </>
         )
