9a31be36ed3c61de9b662cdfb142bcbee24b8604
221B0825 <eunseoyu0825@gmail.com>
2023-11-26 02:03:31 +0900

    image slider

--- src/Pages/MainPage/MainAbout.js
+++ src/Pages/MainPage/MainAbout.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Slider from "./Components/Slider";
+import axios from "axios";
 
 const MainAbout = () => {
-    return(
-        <div>hi</div>
-    )
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = () => {
+        axios.get('https://port-0-promoationpage-server-12fhqa2blnlum4de.sel5.cloudtype.app/api/projects')
+            .then((response) => {
+                const data = response.data;
+                const urlList = [];
+
+                for(let i = 0; i < data.data.length; i++){
+                    urlList.push(data.data[i].imageUrlList[0]);
+                }
+
+                setImages(urlList);
+
+            }).catch((error) => {
+                console.error(error);
+        })
+    }
+
+    return (
+        <Slider images={images}></Slider>
+    );
 }
 
 export default MainAbout;
