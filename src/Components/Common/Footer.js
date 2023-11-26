aa00711897ce88d607004c24a619a5a4f2adac78
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2023-11-26 20:24:46 +0900

    feat: header footer 설정

---
+++ src/Components/Common/Footer.js
@@ -0,0 +1,112 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import Responsive from './responsive';
+import {useNavigate} from "react-router-dom";
+import studioi from "../../assets/logo/mainLogo.png";
+
+const Block = styled.div`
+  width: 100%;
+  padding-top: 3rem;
+  height: 12rem;
+  background-color: #FFA900;
+  z-index: 999;
+  
+  @media(max-width: 970px){
+  padding-top: 2rem;
+    height: 8rem;
+    }
+    
+    @media(max-width: 780px){
+    padding-top: 1rem;
+    height: 4rem;
+    }
+`;
+
+/**
+ * Responsive 컴포넌트의 속성에 스타일을 추가해서 새로운 컴포넌트 생성
+ */
+const Wrapper = styled(Responsive)`
+  width: 80%;
+  height: 90%;
+  background-color: transparent;
+`;
+
+const AdDiv = styled.div`
+    height: 40%;
+`;
+const Div = styled.div`
+    height: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+const Sub = styled.div`
+    width: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+const Margin = styled.div`
+    width: 5%;
+
+`;
+
+const Text = styled.text`
+    font-size: 1.25rem;
+    color: white;
+    white-space: nowrap;
+    margin-bottom: 1rem;
+    cursor: pointer;
+    
+     @media(max-width: 970px){
+    font-size: 1rem;
+    }
+    
+     @media(max-width: 780px){
+    font-size: 0.5rem;
+    }
+`;
+
+const Img = styled.img`
+  height: 3rem;
+  margin-right: 30%;
+  
+  @media(max-width: 970px){
+    height: 2rem;
+    }
+    
+    @media(max-width: 780px){
+    height: 1rem;
+    } 
+`;
+
+
+
+const Footer = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <Block>
+                <Wrapper>
+                    <AdDiv>
+                        <Text>서울시 성동구 광나루로 162 BS성수타워 5층 5F, 162, Gwangnaru-ro, Seongdong-gu, Seoul, Korea</Text>
+                    </AdDiv>
+                    <Div>
+                        <Img src={studioi} alt='logo image'/>
+                        <Sub>
+                        <Text>T. 02-2038-2663</Text>
+                            <Margin/>
+                        <Text>F. 070-7549-2443</Text>
+                        </Sub>
+                    </Div>
+                </Wrapper>
+            </Block>
+
+
+
+        </>
+    );
+};
+
+export default Footer;
--- src/Components/Common/header.js
+++ src/Components/Common/header.js
@@ -6,14 +6,14 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import {motion} from "framer-motion";
 import {Variants} from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import studioi from '../../assets/studioi.png'
+import studioi from '../../assets/logo/mainLogo.png'
 import PlusAboutModal from "../../Pages/AdminPage/Component/PlusAboutModal";
 import AdminModal from "./AdminModal";
 
 const HeaderBlock = styled.div`
   position: fixed;
   width: 100%;
-  background-color: transparent;
+  background-color: #FFA900;
   // box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
    z-index: 999;
 `;
@@ -33,59 +33,14 @@ const AnimatedOffcanvas = styled(Offcanvas)`
     }
 `;
 
-const OffcanvasBody = styled(Offcanvas.Body)`
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  
-  @media(max-width: 840px){
-    padding-left: 0rem;
-    }
-`;
-
-const CanvasDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  width: 95%;
-  height: 70vh;
-  
-  @media(max-width: 840px){
-    padding: 0rem;
-    }
-`;
-const BottomDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  width: 95%;
-  height: 3vh;
-  text-align: right;
-  
-`;
-const Text = styled.text`
-    color: rgba(0,0,0,0.2);
-    font-size: 1.5rem;
-    cursor: default;
-    
-      @media(max-width: 390px){
-    color: rgba(0,0,0,0.0);
-    font-size: 0rem;
-    }
-`;
-/**
- * Responsive 컴포넌트의 속성에 스타일을 추가해서 새로운 컴포넌트 생성
- */
 const Wrapper = styled(Responsive)`
   height: 4rem;
-  width: 100%;
+  width: 85%;
+  padding-left: 5%;
   display: flex;
   align-items: center;
   justify-content: space-between; 
   
-  @media(max-width: 390px){
-    height: 2rem;
-    }
     
   .logo {
     font-weight: 600;
@@ -98,31 +53,34 @@ const Wrapper = styled(Responsive)`
     margin-right: 5%;
   }
 `;
-const StyledHiX = styled(motion(HiX))`
-  font-size: 2.25rem; 
-  cursor: pointer;
-`;
-const StyledHiMenu = styled(motion(HiMenu))`
-  font-size: 2.25rem; 
-  cursor: pointer;
+const Img = styled.img`
+  height: 3rem;
+  
+  @media(max-width: 830px){
+    height: 1.5rem;
+    }
+    
+    @media(max-width: 605px){
+    height: 1.5rem;
+    }
 `;
 
 const MenuButton = styled(motion.button)`
-    width: 20%;
-    font-size: 6rem;
-    font-weight: 800;
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: white;
     border:none;
     outline: none;
     cursor: pointer;
     background-color: transparent;
-    margin-bottom: 5vh;
+    margin: 0 1rem;
     
-    @media(max-width: 670px){
-    font-size: 2.875rem;
+    @media(max-width: 830px){
+     font-size: 1rem;
     }
     
-    @media(max-width: 390px){
-    font-size: 2rem;
+    @media(max-width: 605px){
+    font-size: 0.75rem;
     }
 `;
 
@@ -137,8 +95,7 @@ const MenuButtons = ({ children }) => {
     const buttonVariants = {
         initial: { opacity: 0, x: 0, y: 0 },
         animate: { opacity: 1, x: 40, y: 0, transition:{type: "spring", duration: 1, bounce: 0.5} },
-        hover: {scale: 1.2, color: "blue", transition:{type: "spring", duration: 1, bounce: 0.5}},
-        tab: {scale: 0.9},
+        hover: {scale: 1, color: "black", transition:{type: "spring", duration: 1, bounce: 0.5}},
     };
 
     return (
@@ -150,7 +107,6 @@ const MenuButtons = ({ children }) => {
             animate="animate"
             transition="transition"
             whileHover="hover"
-            whileTap="tab"
         >
             {children}
         </MenuButton>
@@ -197,34 +153,14 @@ const Header = () => {
         <>
             <HeaderBlock>
                 <Wrapper>
-                    <div className="logo"><img src={studioi} alt='logo image' className="SocoaLogo" onClick={goMain}/></div>
+                    <div className="logo"><Img src={studioi} alt='logo image' className="SocoaLogo" onClick={goMain}/></div>
                     <div className="menu">
-                        {show?<StyledHiX className="MenuIcon" onClick={handleClose} />:<StyledHiMenu className="MenuIcon" onClick={handleShow}/>}
-                    </div>
-                </Wrapper>
-            </HeaderBlock>
-
-            <motion.div
-                variants={canvasanimation}
-                initial="initial"
-                animate="animate">
-            <AnimatedOffcanvas show={show} onHide={handleClose} placement="top" >
-                <Offcanvas.Header/>
-                <OffcanvasBody>
-                    <CanvasDiv>
                         <MenuButtons>ABOUT</MenuButtons>
                         <MenuButtons>ARTWORK</MenuButtons>
                         <MenuButtons>CONTACT</MenuButtons>
-                    </CanvasDiv>
-                    <BottomDiv>
-                        <Text onClick={goToAdmin}>manage</Text>
-                    </BottomDiv>
-                    {isAdmin && (
-                        <AdminModal onCancel={handleCancel}/>
-                    )}
-                </OffcanvasBody>
-            </AnimatedOffcanvas>
-            </motion.div>
+                    </div>
+                </Wrapper>
+            </HeaderBlock>
 
 
         </>
