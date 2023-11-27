3db8563036c6f6c7130ecdca40d19a26e6a560a3
221B0825 <eunseoyu0825@gmail.com>
2023-11-27 17:53:26 +0900

    feat: Carousel arrows

--- src/Pages/MainPage/Components/Carousel.js
+++ src/Pages/MainPage/Components/Carousel.js
@@ -21,11 +21,34 @@ const ContentsSlider = styled(Slider)`
         /* padding-right: 5px; */
         /* pointer-events: none; */
     }
+  .slick-prev, .slick-next {
+    width: 50px;
+    height: 50px;
+  }
+
+  .slick-prev:before, .slick-next:before {
+    font-size: 50px;
+    align-items: center;
+    justify-content: center;
+  }
 `;
+
+
 
 const Image = styled.img`
   width: 100%;
 `;
+
+const Pre = styled.div`
+  left: 3%;
+  z-index: 3;
+`;
+
+const NextTo = styled.div`
+  right: 3%;
+  z-index: 3;
+`;
+
 const Carousel = ({images}) => {
     const settings = {
         dots: false,
@@ -33,6 +56,14 @@ const Carousel = ({images}) => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        nextArrow: (
+            <NextTo>{'<'}</NextTo>
+        ),
+        prevArrow: (
+            <Pre>{'>'}</Pre>
+        )
     };
 
     return(
