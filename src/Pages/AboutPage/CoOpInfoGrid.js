71384d6bbd68e8fb6352d2b74bfb8b662a7c2283
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2023-12-02 00:06:23 +0900

    feat: 협력사 변경사항 적용

--- src/Pages/AboutPage/CoOpInfoGrid.js
+++ src/Pages/AboutPage/CoOpInfoGrid.js
@@ -9,6 +9,7 @@ import PD from "../../assets/images/PDRoom.png"
 import See from "../../assets/images/See.png"
 import Urban from "../../assets/images/UrbanPladi.png"
 import Locomo from "../../assets/images/Locomo.png"
+import {useNavigate} from "react-router-dom";
 
 const BoxContainer = styled(motion.div)`
     display: flex;
@@ -36,6 +37,11 @@ const LogoDiv = styled(motion.div)`
     font-weight: 600;
 
 `;
+const ImgClick = styled(motion.img)`
+    width: 25%;
+    height: 70%;
+    cursor: pointer;
+`;
 const Img = styled(motion.img)`
     width: 25%;
     height: 70%;
@@ -45,100 +51,25 @@ const Space = styled.div`
     width: 12.5%;
 `;
 
-const boxVariant = {
-    visible: { opacity: 1, scale: 1, transition : {duration : 1, delay: 0.7 } },
-    hidden: { opacity: 0, scale: 1},
-}
-
 export default function CoOpInfoGrid() {
-    const control = useAnimation();
-    const [ref, inView] = useInView();
-    const control2 = useAnimation();
-    const [ref2, inView2] = useInView();
-
-    const [mainData, setMainData] = useState([]);
-    const [subData, setSubData] = useState([]);
-
-    const goManagement = (link) => {
-        window.location.href = `${link}`;
+    const PladiClick = () => {
+        window.location.href = "https://pladi.tv/";
     };
-
-    useEffect(()=>{
-
-        axios.get('https://port-0-promoationpage-server-12fhqa2blnlum4de.sel5.cloudtype.app/api/partners')
-
-            .then(response => {
-                const data = response.data;
-                console.log(data);
-                console.log(data.data[0]);
-                const objects = [];
-                const objects2 = [];
-
-                for(let i = 0; i < data.data.length; i++) {
-
-                    if(data.data[i].is_main){
-                        const obj = {
-                            id: data.data[i].id,
-                            logoImageUrl: data.data[i].logoImageUrl,
-                            link: data.data[i].link,
-                            is_main: "Main CoOp."
-                        };
-
-                        console.log("여기");
-                        console.log(obj);
-                        objects.push(obj);
-                    }
-                    else {
-                        const obj = {
-                            id: data.data[i].id,
-                            logoImageUrl: data.data[i].logoImageUrl,
-                            link: data.data[i].link,
-                            is_main: "Sub CoOp."
-                        };
-
-                        console.log("여기22");
-                        console.log(obj);
-                        objects2.push(obj);
-                    }
-                }
-                setMainData(objects);
-                setSubData(objects2);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    },[]);
-
-    useEffect(() => {
-        if (inView) {
-            control.start("visible");
-        } else {
-            control.start("hidden");
-        }
-
-    }, [control, inView]);
-
-    useEffect(() => {
-        if (inView2) {
-            control2.start("visible");
-        } else {
-            control2.start("hidden");
-        }
-
-    }, [control2, inView2]);
-
+    const SeeClick = () => {
+        window.location.href = "https://www.seeutter.com/channels/L2NoYW5uZWxzLzIxMjg/pages/home";
+    };
   return (
       <BoxContainer>
           <Title>CORP.</Title>
           <LogoDiv>
-              <Img src={Pladi}></Img>
+              <ImgClick src={Pladi} onClick={PladiClick} ></ImgClick>
               <Space />
               <Img src={Eye}></Img>
               <Space />
               <Img src={PD}></Img>
           </LogoDiv>
           <LogoDiv>
-              <Img src={See}></Img>
+              <ImgClick src={See} onClick={SeeClick}></ImgClick>
               <Space />
               <Img src={Urban}></Img>
               <Space />
