fcefc9a896f8ce022c3fa917b5e2245d2b1b4f7a
221B0825 <eunseoyu0825@gmail.com>
2023-12-01 23:10:38 +0900

    fix: useEffect

--- src/Pages/DetailPage/ContentDetailPage.js
+++ src/Pages/DetailPage/ContentDetailPage.js
@@ -29,6 +29,8 @@ const ContentDetailPage = () => {
     const [imageList, setImageList] = useState([]);
     const [youtubePlayer, setYoutubePlayer] = useState(null);
 
+    const APIKey = "";
+
     useEffect(() => {
         getDetails(id);
     }, [id]);
@@ -60,7 +62,7 @@ const ContentDetailPage = () => {
 
     const imageBanner = imageList.map((image, index) => (
         <div key={index}>
-            <img src={image} alt={`Image ${index}`} />
+            <img src={image} alt={`Slide ${index}`} />
         </div>
     ));
 
@@ -76,48 +78,45 @@ const ContentDetailPage = () => {
     };
 
     useEffect(() => {
-        renderVideoPlayer();
-    }, [detail.link, youtubePlayer]);
-
-
-    useEffect(() => {
-        const determineVideoType = async () => {
-            try {
-                if (detail.link) {
-                    if (detail.link.includes("/@")) {
-                        // 채널의 가장 최근 영상
-                        const channelUsername = detail.link.split("/@")[1];
-                        const channelIdResponse = await axios.get(`https://www.googleapis.com/youtube/v3/channels?key=YOUR_YOUTUBE_API_KEY&forUsername=${channelUsername}&part=id`);
-                        const channelId = channelIdResponse.data.items[0].id;
-                        const latestVideoResponse = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=YOUR_YOUTUBE_API_KEY&channelId=${channelId}&part=id&order=date&type=video`);
-                        const latestVideoId = latestVideoResponse.data.items[0].id.videoId;
-                        setDetail({ ...detail, link: latestVideoId });
-                    } else if (detail.link.includes("/playlist?list=")) {
-                        // 재생목록의 첫 번째 영상
-                        const playlistId = detail.link.split("list=")[1];
-                        const playlistResponse = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?key=YOUR_YOUTUBE_API_KEY&playlistId=${playlistId}&part=snippet&maxResults=1`);
-                        const firstVideoId = playlistResponse.data.items[0].snippet.resourceId.videoId;
-                        setDetail({ ...detail, link: firstVideoId });
-                    } else if (detail.link.includes("/user/")) {
-                        // 채널의 가장 최근 영상
-                        const channelUsername = detail.link.split("/user/")[1];
-                        const channelIdResponse = await axios.get(`https://www.googleapis.com/youtube/v3/channels?key=YOUR_YOUTUBE_API_KEY&forUsername=${channelUsername}&part=id`);
-                        const channelId = channelIdResponse.data.items[0].id;
-                        const latestVideoResponse = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=YOUR_YOUTUBE_API_KEY&channelId=${channelId}&part=id&order=date&type=video`);
-                        const latestVideoId = latestVideoResponse.data.items[0].id.videoId;
-                        setDetail({ ...detail, link: latestVideoId });
-                    } else if (detail.link.includes("youtu.be")) {
-                        // 동영상 주소
-                        const videoId = detail.link.split("youtu.be/")[1];
-                        setDetail({ ...detail, link: videoId });
-                    }
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
+        determineVideoType();
     }, [detail.link]);
+
+    const determineVideoType = async () => {
+        try {
+            if (detail.link) {
+                if (detail.link.includes("/@")) {
+                    // 채널의 가장 최근 영상
+                    const channelUsername = detail.link.split("/@")[1];
+                    const channelIdResponse = await axios.get(`https://www.googleapis.com/youtube/v3/channels?key=${APIKey}&forUsername=${channelUsername}&part=id`);
+                    const channelId = channelIdResponse.data.items[0].id;
+                    const latestVideoResponse = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${APIKey}&channelId=${channelId}&part=id&order=date&type=video`);
+                    const latestVideoId = latestVideoResponse.data.items[0].id.videoId;
+                    setDetail({ ...detail, link: latestVideoId });
+                } else if (detail.link.includes("/playlist?list=")) {
+                    // 재생목록의 첫 번째 영상
+                    const playlistId = detail.link.split("list=")[1];
+                    const playlistResponse = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?key=${APIKey}&playlistId=${playlistId}&part=snippet&maxResults=1`);
+                    const firstVideoId = playlistResponse.data.items[0].snippet.resourceId.videoId;
+                    setDetail({ ...detail, link: firstVideoId });
+                } else if (detail.link.includes("/user/")) {
+                    // 채널의 가장 최근 영상
+                    const channelUsername = detail.link.split("/user/")[1];
+                    const channelIdResponse = await axios.get(`https://www.googleapis.com/youtube/v3/channels?key=${APIKey}&forUsername=${channelUsername}&part=id`);
+                    const channelId = channelIdResponse.data.items[0].id;
+                    const latestVideoResponse = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${APIKey}&channelId=${channelId}&part=id&order=date&type=video`);
+                    const latestVideoId = latestVideoResponse.data.items[0].id.videoId;
+                    setDetail({ ...detail, link: latestVideoId });
+                } else if (detail.link.includes("youtu.be")) {
+                    // 동영상 주소
+                    const videoId = detail.link.split("youtu.be/")[1];
+                    console.log(videoId)
+                    setDetail({ ...detail, link: videoId });
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
         <Body>
